cc0e4968e11d07148f5ecaca0fa4eb14dcc810ee
Arya <aryaamoriya@gmail.com>
2026-06-12 09:04:32 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-
+  baseURL: "https://ai-startup-validator-rb81.onrender.com/api",
 });
 
 export default api;
+
+//locally
+
+//import axios from "axios";
+
+//const api = axios.create({
+//  baseURL: "http://localhost:5000/api",
+//});
+
+//export default api;
